3561f0b734dabc28265530b744352fabb55e1133
speediplayz <43318169+speediplayz@users.noreply.github.com>
2021-06-16 11:45:37 +0100

    Update Form1.cs

--- SE Blueprints/Form1.cs
+++ SE Blueprints/Form1.cs
@@ -17,33 +17,6 @@ namespace SE_Blueprints
         public Form1()
         {
             InitializeComponent();
-            /*
-            Blueprint bp = new Blueprint("StalinMan", @"C:\Users\jakub\OneDrive\Desktop", false);
-
-            
-            bp.AddBlock(0, 0, 0, new Vector3(255,   0,   0));
-            bp.AddBlock(0, 1, 0, new Vector3(255, 255,   0));
-            bp.AddBlock(0, 2, 0, new Vector3(255, 255, 255));
-            bp.AddBlock(0, 3, 0, new Vector3(0,   255,   0));
-            bp.AddBlock(0, 4, 0, new Vector3(0,   255, 255));
-            bp.AddBlock(0, 5, 0, new Vector3(0,   0,   255));
-            bp.AddBlock(0, 6, 0, new Vector3(0,   0,   0  ));
-            
-
-            for(int x = 0; x < image.Width; x++)
-            {
-                for(int y = 0; y < image.Height; y++)
-                {
-                    Color c = image.GetPixel(x, y);
-                    if(c.A > 10)
-                    {
-                        bp.AddBlock(x, y, 0, new Vector3(c.R, c.G, c.B));
-                    }
-                }
-            }
-
-            bp.Save();
-            */
         }
 
         private void cmdStart_Click(object sender, EventArgs e)
